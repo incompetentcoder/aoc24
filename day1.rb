647bcaedbe2c818caafa47e24bf79246a16ceb1e
incompetentcoder <demonicmaniag@googlemail.com>
2024-12-05 03:13:10 +0100

    day1 part2

--- day1.rb
+++ day1.rb
@@ -1,2 +1,5 @@
-pp File.open("day1input.txt").read.split("\n").map {|x| x.split(" ").map(&:to_i)}.transpose.map {|x| x.sort}.transpose.map {|x| (x[0]-x[1]).abs}.sum
+a=File.open("day1input.txt").read.split("\n").map {|x| x.split(" ").map(&:to_i)}.transpose
 
+pp a.map {|x| x.sort}.transpose.map {|x| (x[0]-x[1]).abs}.sum
+
+pp a[0].map {|x| x*a[1].count(x)}.sum
