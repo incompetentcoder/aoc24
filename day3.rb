3b6510d66ef8a1d65f19c5e2191cafd5624afd76
incompetentcoder <demonicmaniag@googlemail.com>
2024-12-06 02:09:42 +0100

    typos day3p1 fixed, day3p2

--- day3.rb
+++ day3.rb
@@ -1,4 +1,7 @@
 a=File.open("day3input.txt").read
 
-pp a.scan(/mul\(\d+,\d+\)/).map(&:to_i).reduce(:*)}.sum
+pp a.scan(/mul\(\d+,\d+\)/).map {|x| x.scan(/\d+/).map(&:to_i).reduce(:*)}.sum
+
+pp ("do()"+a).split("don\'t()").map {|x| x.gsub(/.*do()/,"")}.join.scan(/mul\(\d+,\d+\)/).map {|x| x.scan(/\d+/).map(&:to_i).reduce(:*)}.sum
+
 
