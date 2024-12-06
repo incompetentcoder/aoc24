1ef9fb0603b04b5074f055578fc19a67a2b891f5
incompetentcoder <demonicmaniag@googlemail.com>
2024-12-06 01:12:37 +0100

    day2 part2 ugly

--- day2.rb
+++ day2.rb
@@ -17,4 +17,25 @@ end
 pp correct
 
 
+correct=0
+correct2=0
+a.each do |x|
+  hm = x.each_cons(2).map {|y,z| y > z}.tally
+  x.reverse! if (hm[false] || 0) < 2
+  x.each_index do |y|
+    correct2=1
+    x.values_at(*((0...x.length).to_a - [y])).each_cons(2) do |y1,y2|
+      unless (y2 > y1) && (y2 - y1).between?(1,3)
+        correct2=0
+        break
+      end
+    end
+    if correct2==1
+      correct+=1
+      break
+    end
+  end
+end
+
+pp correct
 
