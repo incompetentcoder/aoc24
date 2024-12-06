cc8d72ad8dc378775c9941ae0b9c66677d1e8f8d
incompetentcoder <demonicmaniag@googlemail.com>
2024-12-06 03:53:34 +0100

    fix day3p2

--- day3.rb
+++ day3.rb
@@ -2,6 +2,13 @@ a=File.open("day3input.txt").read
 
 pp a.scan(/mul\(\d+,\d+\)/).map {|x| x.scan(/\d+/).map(&:to_i).reduce(:*)}.sum
 
-pp ("do()"+a).split("don\'t()").map {|x| x.gsub(/.*do()/,"")}.join.scan(/mul\(\d+,\d+\)/).map {|x| x.scan(/\d+/).map(&:to_i).reduce(:*)}.sum
+ind=0
+res=0
+while (b=a.index("don't()",ind)) != nil do
+  res+=a[ind..b].scan(/mul\(\d+,\d+\)/).map {|x| x.scan(/\d+/).map(&:to_i).reduce(:*)}.sum
+  ind=a.index("do()",b)
+  break if ind==nil
+end
+pp res
 
 
