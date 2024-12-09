cef5f9d6bb1c2211354652e7ff480e09e0d48b81
incompetentcoder <demonicmaniag@googlemail.com>
2024-12-09 08:44:58 +0100

    much faster day9p1

--- day9.rb
+++ day9.rb
@@ -1,6 +1,10 @@
 a=File.open("day9input.txt").read.chars.map(&:to_i).each_with_index.each_slice(2).map {|x,y| [[(x[1]/2)]*x[0],["."]*y[0]]}.flatten
-
-while b=a.index(".") do
-  a[b]=a.pop
+b=a.each_with_index.find_all{|x| x[0]=="."}
+b.each do |x|
+  break if x[1] > a.length
+  while a[-1].class != Integer do
+    a.pop
+  end
+  a[x[1]]=a.pop
 end
 pp a.each_with_index.map{|x,y| x*y}.sum  
